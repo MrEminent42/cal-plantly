c51ae8faae5c606b0ecca83665523e210c2e2183
MrEminent42 <github@leohorwitz.com>
2023-05-13 10:45:47 -0700

    Initial commit to set up repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome to Cal Plantly!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
