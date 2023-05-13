e6c5836c8422dbb03714d589ecef7a208350483a
MrEminent42 <github@leohorwitz.com>
2023-05-13 13:18:30 -0700

    colors and formatting

--- frontend/src/components/TopBar/TopBar.tsx
+++ frontend/src/components/TopBar/TopBar.tsx
@@ -6,28 +6,32 @@ import Box from '@mui/material/Box';
 
 const TopBar = () => {
     return (
-        <AppBar position="static" sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-        }}>
-            <Chip sx={{ backgroundColor: 'red', flex: 1 }}>
+        <AppBar position="static"
+            elevation={0}
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                background: 'linear-gradient(to bottom, rgb(237, 229, 128), rgba(237, 229, 128, 0.1))',
+                paddingBottom: '10px',
+            }}>
+            <Chip sx={{ flex: 1 }}>
                 Weather
             </Chip>
-            <Chip sx={{ backgroundColor: 'red', flex: 1 }}>
+            <Chip sx={{ flex: 1 }}>
                 Points
             </Chip>
-            <Chip sx={{ backgroundColor: 'red', flex: 1 }}>
+            <Chip sx={{ flex: 1 }}>
                 hi
             </Chip>
-        </AppBar>
+        </AppBar >
     )
 }
 
 export default TopBar
 
 const Chip = styled(`div`)`
-    background-color: #885cf7;
+    background-color: #a4af69;
     border-radius: 10px;
     padding: 10px;
     margin: 10px;
