d411b194c6e41108b974c6abed06bd56d9c9111f
MrEminent42 <github@leohorwitz.com>
2023-05-14 02:10:31 -0700

    heroku troubleshooting

--- backend/scripts/heroku-start.js
+++ backend/scripts/heroku-start.js
@@ -4,16 +4,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
 // Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
 // Root Redirects to the pre-build assets
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'build'));
+    res.sendFile(path.join(__dirname, '..', 'dist'));
 });
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+    res.sendFile(path.join(__dirname, '..', 'dist/index.html'));
 });
+
 app.listen(port, () => {
     console.log("Server is running on port: ", port)
 })
