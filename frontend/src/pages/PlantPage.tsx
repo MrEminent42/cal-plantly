a3fcd8a184a67226f6e637401a7892bdb0cc0fd9
lindlands <lindlands@outlook.com>
2023-05-13 21:38:40 -0700

    plant page made for red valerian, +font +images

---
+++ frontend/src/pages/PlantPage.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled'
+import { Box } from '@mui/system'
+import "./BookStyle.css";
+
+
+const PlantPage = () => {
+    return (
+        <HomeDiv style={{
+            flexGrow: 1,
+            flexDirection: 'column'
+        }}>
+            <div className="book-title">Red Valerian</div>
+            <div className='main'>
+                
+                <div className='leftside'>
+                    <div className='plimage'>
+                    <img src="/images/red valerian.png" height="500" />
+                    </div>
+                </div>
+                
+                <div className='maincontainer'>
+                <div className='notes'>
+                Red Valerian<br />
+                ‣ Centranthus ruber<br />
+                ‣ Full sun<br />
+                ‣ 32 inches<br />
+                ‣ Sandy or rocky soil<br />
+                ‣ Fertilize once when planted<br />
+                <br />
+                Initially from the Mediterranian region, this perennial attracts both bees and butterflies.<br />
+                </div>
+                </div>
+            </div>
+            
+        </HomeDiv>
+        
+
+    )
+}
+
+export default PlantPage
+
+const HomeDiv = styled(Box)(() => ({
+    display: 'flex',
+}))
