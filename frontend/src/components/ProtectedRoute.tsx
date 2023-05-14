34fc6632ee54880d81b3e2e8b5f939ad6e716d38
MrEminent42 <github@leohorwitz.com>
2023-05-14 04:00:36 -0700

    update loading screen

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -5,6 +5,7 @@ import { useAtom } from 'jotai';
 import { useState, useEffect } from 'react';
 import { Navigate } from "react-router-dom";
 import { AuthState, currentUserAtom } from '../jotai/authAtoms';
+import { Typography } from '@mui/material';
 // import { firebaseAuth } from '../config/firebase';
 
 
@@ -24,15 +25,7 @@ export default function ProtectedRoute({ children }: { children: JSX.Element }) 
                     transitionDelay: '800ms',
                 }}
             >
-                <Box sx={{ width: { xs: '100%', md: '600px' } }}>
-                    <Box sx={{ px: '1rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <Skeleton variant="rectangular" height={'50px'} width={'80%'} sx={{ my: '15px', borderRadius: '20px' }} />
-                        <Skeleton variant="rectangular" height={'130px'} width={'100%'} sx={{ my: '15px', borderRadius: '20px' }} />
-                        <Skeleton variant="rectangular" height={'130px'} width={'100%'} sx={{ my: '15px', borderRadius: '20px' }} />
-                        <Skeleton variant="rectangular" height={'130px'} width={'100%'} sx={{ my: '15px', borderRadius: '20px' }} />
-                        <Skeleton variant="rectangular" height={'130px'} width={'100%'} sx={{ my: '15px', borderRadius: '20px' }} />
-                    </Box >
-                </Box>
+                <Typography>Loading ;)</Typography>
             </Fade>
         )
     }
