78e4a09320870646b410eec6ac6d1ad4bb2ecc42
hannah <113932134+hanmosh@users.noreply.github.com>
2023-05-13 19:49:06 -0700

    working geolocation marker on map

---
+++ frontend/src/api/getLocation.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+}
+
+export default App;
