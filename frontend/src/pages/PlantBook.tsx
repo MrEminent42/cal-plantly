185e34955e58bc309e4736928e82ad531a923384
lindlands <lindlands@outlook.com>
2023-05-13 23:29:37 -0700

    basic plant book

--- frontend/src/pages/PlantBook.tsx
+++ frontend/src/pages/PlantBook.tsx
@@ -1,23 +1,39 @@
 import styled from '@emotion/styled'
-import React from 'react'
+import Grid from '@mui/material/Grid';
 import { Box } from '@mui/system'
+import PlantEntry from '../components/PlantBook/PlantEntry';
+import "./BookStyle.css";
 
-const Home = () => {
+
+const PlantBook = () => {
     return (
         <HomeDiv style={{
             flexGrow: 1,
+            flexDirection: 'column'
         }}>
 
-            PlantBook
-
+            <div className="book-title">Plant Book</div>
+            
+            <div className='main-box'>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {Array.from(Array(3)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <PlantEntry id = {index}></PlantEntry>
+                    </Grid>
+                ))}
+                </Grid>
+            </div>
+            
         </HomeDiv>
+        
+
     )
 }
 
-export default Home
+export default PlantBook
 
 const HomeDiv = styled(Box)(() => ({
     display: 'flex',
-    border: '1px solid red',
+    paddingBottom: '50px',
     flex: 1,
 }))
