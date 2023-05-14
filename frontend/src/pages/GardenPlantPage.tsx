d46c9feb4a017e40cc65d2d788bfc158b0593e17
MrEminent42 <github@leohorwitz.com>
2023-05-14 07:18:05 -0700

    small fix typo

--- frontend/src/pages/GardenPlantPage.tsx
+++ frontend/src/pages/GardenPlantPage.tsx
@@ -2,13 +2,13 @@ import styled from '@emotion/styled'
 import React, { useEffect, useState } from 'react'
 import { Box } from '@mui/system'
 import { useParams } from 'react-router-dom'
-import { PlantInfo as GardenPlantPage, getPlant, getPlants } from '../api/plants'
+import { PlantInfo, getPlant, getPlants } from '../api/plants'
 
 const GardenPlantPage = () => {
     const params = useParams();
     // if id is undefined or invalid, redirect to plant book
 
-    const [plants, setPlants] = useState<GardenPlantPage | null>(null);
+    const [plants, setPlants] = useState<PlantInfo | null>(null);
 
     useEffect(() => {
         if (params.id === undefined || params.id === null) return;
