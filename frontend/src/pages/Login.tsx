248e419d35b7dc531bdd6c4c4bbc7481cdf70fd7
lindlands <lindlands@outlook.com>
2023-05-14 08:12:25 -0700

    login looks nicer

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,6 +9,7 @@ import { currentUserAtom } from '../jotai/authAtoms';
 import { Backdrop, Button, Card, CircularProgress, Fade, Typography } from '@mui/material';
 
 import GoogleIcon from '@mui/icons-material/Google';
+import "./LoginStyle.css";
 
 
 const Login = () => {
@@ -40,40 +41,18 @@ const Login = () => {
 
     const renderSignIn = () => {
         return (
-            <Box sx={{ width: { xs: '100%', md: '600px' }}}>
-                <Fade in>
-                    <Box sx={{
-                        position: 'absolute',
-                        top: 0,
-                        bottom: 0,
 
-                        // bottom: '30rem',
-                        // right: '30vw',
-                        // left: '30vw',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        flexDirection: 'column'
-                    }}>
-                        <h1>Cal Plantly</h1>
-
-                        <Button onClick={signInWithGoogle} variant="contained" color="secondary" >
-                            <GoogleIcon sx={{ mr: '1rem' }} />
-                            Sign in with Google
-                        </Button>
-
-                    </Box>
-
-
-
-                </Fade>
-
-                <Backdrop
-                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                    open={loading}
-                >
-                    <CircularProgress color="secondary" />
-                </Backdrop>
-            </Box >
+                        <div className='full-page'>
+                            <div className='card'>
+                            <div className='title'>Cal Plantly</div>
+                            <div className='button-section'>
+                                <Button onClick={signInWithGoogle} variant="contained" color="secondary" >
+                                    <GoogleIcon sx={{ mr: '1rem' }} />
+                                    Sign in with Google
+                                </Button>
+                            </div>
+                            </div>
+                        </div>
         )
     }
 
