f9b28c0d2e3d91bdb4adf6069fdc0ab74ddeba83
MrEminent42 <github@leohorwitz.com>
2023-05-13 21:25:19 -0700

    tabs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,65 +20,65 @@ import ActionButton from './components/ActionButton';
 import Map from './components/Map/Map';
 
 function App() {
-    const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
+  const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
 
-    const setupFirebase = () => {
-        // Import the functions you need from the SDKs you need
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
+  const setupFirebase = () => {
+    // Import the functions you need from the SDKs you need
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
 
-        // Your web app's Firebase configuration
-        const firebaseConfig = {
-            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-            storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.REACT_APP_FIREBASE_APP_ID,
-        };
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    };
 
-        // Initialize Firebase
-        const firebaseApp = initializeApp(firebaseConfig);
-        const firebaseAuth = getAuth();
+    // Initialize Firebase
+    const firebaseApp = initializeApp(firebaseConfig);
+    const firebaseAuth = getAuth();
 
-        onAuthStateChanged(firebaseAuth, (user) => {
-            if (user) {
-                // User is signed in, see docs for a list of available properties
-                // https://firebase.google.com/docs/reference/js/firebase.User
-                const uid = user.uid;
-                setCurrentUser(user)
+    onAuthStateChanged(firebaseAuth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setCurrentUser(user)
 
-            } else {
-                setCurrentUser(AuthState.LOGGED_OUT)
-            }
-        });
+      } else {
+        setCurrentUser(AuthState.LOGGED_OUT)
+      }
+    });
 
-    }
+  }
 
-    setupFirebase();
+  setupFirebase();
 
-    const [showBook, setShowBook] = useState(true);
+  const [showBook, setShowBook] = useState(true);
 
-    return (
-        <div style={{
-            border: '1px dotted blue',
-            height: '99vh',
-            display: 'flex',
-            flexDirection: 'column',
-        }}>
-            <ThemeProvider theme={theme}>
+  return (
+    <div style={{
+      border: '1px dotted blue',
+      height: '99vh',
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <ThemeProvider theme={theme}>
 
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-                        <Route path="/plantbook" element={<ProtectedRoute><PlantBook /></ProtectedRoute>} />
-                        <Route path="/login" element={<Login />} />
-                    </Routes>
-                </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/plantbook" element={<ProtectedRoute><PlantBook /></ProtectedRoute>} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
 
-            </ThemeProvider>
-        </div>
-    );
+      </ThemeProvider>
+    </div>
+  );
 }
 
 
